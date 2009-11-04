5e49563fb00669696c687b3ecb1e66fc08add642
Seth Woodworth <seth@isforinsects.com>
2009-11-04 01:05:59 -0500

    adding database interactions

--- talertparser.py
+++ talertparser.py
@@ -2,6 +2,36 @@ from BeautifulSoup import BeautifulStoneSoup
 import urllib
 from datetime import datetime,date
 import time
+import sqlalchemy
+
+#database config
+engine = create_engine('sqlite:///talerts.sql')
+
+metadata = MetaData()
+talerts_table = Table('talerts', metadata,
+	Column('id', Integer, primary_key=True),
+	Column('guid', String),
+	Column('title', String),
+	Column('content', String),
+	Column('mbta_date', Date),
+	Column('insert_date', Date)
+)
+
+metadata.create_all(engine) 
+
+class Talert(object):
+	def __init__(self, guid, title, content, mbta_date, insert_date):
+		self.guid = guid
+		self.title = title
+		self.content = content
+		self.mbta_date = mbta_date
+		self.insert_date = insert_date
+
+	def __repr__(self):
+		return "<Talert('%s','%s','%s','%s','%s')>" % (self.guid, self.title, self.content, self.mbta_date, self.insert_date)
+
+
+mapper(Talert, talerts_table)
 
 url = 'http://talerts.com/rssfeed/alertsrss.aspx'
 response = urllib.urlopen(url)
@@ -12,23 +42,23 @@ xmlSoup = BeautifulStoneSoup(raw_xml)
 
 
 def parse_item(item):
-	"""Return the elements of the items"""
+	#Return the elements of the items
 	guid = item.find("guid").find(text=True)
 	date_str = item.find("pubdate").find(text=True)
 	title = item.find("title").find(text=True)
-	desc = item.find("description").find(text=True)
+	content = item.find("description").find(text=True)
 	
 	date_format = datetime.strptime(date_str, '%a, %d %b %Y %H:%M:%S GMT')
 	date = time.mktime(date_format.timetuple())
 
 	return {'guid':guid,
 			'title':title,
-			'desc':desc,
+			'content':content,
 			'date':date
 			}
 
 def item_block(soup):
-	"""Take xml and generate items"""
+	#Take xml and generate items
 	for ch in soup.findAll('item'):
 		print parse_item(ch)
 
